2a3d86d83086a70dac0c7433e5f738be5271fc99
builtbyjosh <pc.refuge@gmail.com>
2022-09-03 23:00:27 -0500

    change to jsx files

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import {useRouter} from 'next/router'
 
 export default function EventPage() {
-
+  const router = useRouter()
+  console.log('ROUTER: ', router);
   return (
     <Layout>
       <h1>My Event</h1>
