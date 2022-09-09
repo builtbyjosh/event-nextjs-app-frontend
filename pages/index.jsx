407e3f2a1ee6a18972a8796de3d8da817ff4673b
builtbyjosh <pc.refuge@gmail.com>
2022-09-09 10:55:16 -0500

    set up initial data fetching

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,20 @@
 import Head from 'next/head'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({events}) {
+  console.log('EVENTS: ', events)
   return (
     <Layout>
-      <h1>Home Page</h1>      
+      <h1>Upcomming Events</h1>      
     </Layout>
   )
+}
+
+export async function getServerSideProps(){
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: {events}
+  }
 }
