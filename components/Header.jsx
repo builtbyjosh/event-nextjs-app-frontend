2c5634c18c139b3cbfa5bfa4f2e55b8de91179a6
builtbyjosh <pc.refuge@gmail.com>
2022-09-16 19:53:48 -0500

    initial form setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from '@/styles/Header.module.css'
-import Search from './Search'
+import React from 'react';
+import Link from 'next/link';
+import styles from '@/styles/Header.module.css';
+import Search from './Search';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href='/'>
-        <a href="">Music Events Project</a>
+        <Link href="/">
+          <a href="">Music Events Project</a>
         </Link>
       </div>
 
@@ -17,12 +17,17 @@ export const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link href='/events'>
+            <Link href="/events">
               <a href="">Events</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/events/add">
+              <a>Add Event</a>
             </Link>
           </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
--- pages/events/add.jsx
+++ pages/events/add.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import {useState} from 'react';
 import Layout from '@/components/Layout';
+import { useRouter } from 'next/router';
+import { API_URL } from '@/config/index';
+import Link from 'next/link';
+import styles from '@/styles/Form.module.css'
+
 
 export default function AddEventPage() {
   return (
