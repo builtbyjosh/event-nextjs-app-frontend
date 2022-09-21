4693c70b158644c1da45d96abb5b501235871c96
builtbyjosh <pc.refuge@gmail.com>
2022-09-21 00:20:48 -0500

    fix href

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -46,7 +46,7 @@ export default function EventPage({ evt }) {
     <Layout>
       <div className={styles.event}>
         <div className={styles.controles}>
-          <Link href={`/events/edits/${evt.id}`}>
+          <Link href={`/events/edit/${evt.id}`}>
             <a>
               <FaPencilAlt /> Edit Event
             </a>
