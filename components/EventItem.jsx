982ecf1085f799ef71f86d52d4848d1f0ef97e89
builtbyjosh <pc.refuge@gmail.com>
2022-09-22 00:48:52 -0500

    add default image

--- components/EventItem.jsx
+++ components/EventItem.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '@/styles/EventItem.module.css';
+import defaultImage from '../../../public/images/event-default.png';
 
 export default function EventItem({ evt }) {
-  const { image, date, time, name, slug } = evt.attributes
+  const { image, date, time, name, slug } = evt.attributes;
   // const thumbnail = image.data.attributes.url
-  const thumbnail = image.data !== null ? image.data.attributes.url : undefined;
+  const thumbnail =
+    image.data !== null ? image.data.attributes.url : defaultImage;
   return (
     <div className={styles.event}>
       <div className={styles.img}>
@@ -17,14 +19,16 @@ export default function EventItem({ evt }) {
         />
       </div>
       <div className={styles.info}>
-        <span>{new Date(date).toLocaleDateString('en-US')} at {time}</span>
+        <span>
+          {new Date(date).toLocaleDateString('en-US')} at {time}
+        </span>
         <h3>{name}</h3>
       </div>
       <div className={styles.link}>
         <Link href={`/events/${slug}`}>
-          <a className='btn' >Details</a>
+          <a className="btn">Details</a>
         </Link>
       </div>
     </div>
   );
-};
+}
